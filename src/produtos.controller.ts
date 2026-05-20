9b9ab25def8dfef605f3fe3a33eb6df3c1bb2a4b
enzin00-eng <fabio.enzoo00@gmail.com>
2026-05-20 14:28:01 -0300

    First commit

---
+++ src/produtos.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from "./produto.model";import { ProdutosServices } from "./produtos.service";
+;
+
+@Controller('produtos')
+export class ProdutosController {
+    constructor(private produtosServices: ProdutosServices) {
+
+    }    
+    
+    @Get()
+    obterTodos(): Produto[] {
+        return this.produtosServices.obterTodos();
+    }
+    
+    @Get(':id')
+    obterUm(@Param() params): Produto {
+        return this.produtosServices.obterUm(params.id);
+    }
+
+    @Post() 
+    criar(@Body() produto: Produto) {
+        this.produtosServices.criar(produto);
+    }
+
+    @Put()
+    alterar(@Body() produto: Produto): Produto {
+        return this.produtosServices.alterar(produto);
+    }
+
+    @Delete(':id')
+    apagar(@Param() params) {
+        this.produtosServices.apagar(params.id);
+    }
+}
---
+++ src/produtos.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { Produto } from "./produto.model";
+
+@Injectable()
+export class ProdutosServices {
+    produtos: Produto[] = [
+        new Produto("LIV01", "Livro TDD E BDD na prática", 29.90),
+        new Produto("LIV02", "Iniciando com Flutter", 39.90),
+        new Produto("LIV03", "ChatGPT como serviço", 29.90),
+    ];
+
+    obterTodos(): Produto[] {
+        return this.produtos;
+    }
+
+    obterUm(id: number): Produto {
+        return this.produtos[0];
+    }
+
+    criar(produto: Produto) {
+        this.produtos.push;
+    }
+
+    alterar(produto: Produto): Produto {
+        return produto;
+    }
+
+    apagar(id: number) {
+        this.produtos.pop();
+    }
+}
